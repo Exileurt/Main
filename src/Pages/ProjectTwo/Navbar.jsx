7cd9ef271ec9a97e96d28247928eb175ca7e3aa0
Exileurt <favrel.aurelien@gmail.com>
2023-09-14 11:08:12 +0200

    update

--- src/Pages/ProjectTwo/Navbar.jsx
+++ src/Pages/ProjectTwo/Navbar.jsx
@@ -1,33 +1,23 @@
+import { linkNavbar } from "./ProjectTwoData";
 import logo from "../../assets/Projecttwo/logo.svg";
-
-const linkNavbar = [
-  { href: "/", label: "About" },
-  { href: "/careers", label: "Careers" },
-  { href: "/events", label: "Events" },
-  { href: "/product", label: "Product" },
-  { href: "/support", label: "Support" },
-];
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex justify-between'>
       <img src={logo} alt='logo' className='w-40' />
-
-      <nav className=''>
-        <ul className='flex space-x-6'>
-          {linkNavbar.map((link, index) => (
-            <li key={index}>
-              <a
-                href={link.href}
-                className='relative transition ease-linear group'
-              >
-                {link.label}
-                <span className='absolute inset-x-0 bottom-0 h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform origin-center'></span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <ul className='flex space-x-8'>
+        {linkNavbar.map((link, index) => (
+          <li key={index}>
+            <a
+              href={link.href}
+              className='relative transition ease-linear group'
+            >
+              {link.label}
+              <span className='absolute bg-white h-0.5 scale-x-0 bottom-0 inset-x-0 transition-transform group-hover:scale-x-100 origin-center' />
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Pages/ProjectTwo/ProjectTwoData.js
@@ -0,0 +1,27 @@
+import curiosity from "../../assets/Projecttwo/desktop/image-curiosity.jpg";
+import deepEarth from "../../assets/Projecttwo/desktop/image-deep-earth.jpg";
+import fishEye from "../../assets/Projecttwo/desktop/image-fisheye.jpg";
+import fromAbove from "../../assets/Projecttwo/desktop/image-from-above.jpg";
+import nightArcade from "../../assets/Projecttwo/desktop/image-night-arcade.jpg";
+import pocketBorealis from "../../assets/Projecttwo/desktop/image-pocket-borealis.jpg";
+import soccerImage from "../../assets/Projecttwo/desktop/image-soccer-team.jpg";
+import porsche from "../../assets/Projecttwo/desktop/image-grid.jpg";
+
+export const imagesWithText = [
+  { src: deepEarth, alt: "deepearth", text: "Deep Earth" },
+  { src: nightArcade, alt: "nightarcade", text: "Night Arcade" },
+  { src: soccerImage, alt: "soccerimage", text: "Soccer Image" },
+  { src: porsche, alt: "porsche", text: "Porsche" },
+  { src: fromAbove, alt: "fromAbove", text: "From Up Above vr" },
+  { src: pocketBorealis, alt: "pocketborealis", text: "Pocket Borealis" },
+  { src: curiosity, alt: "curiosity", text: "Curiosity" },
+  { src: fishEye, alt: "fisheye", text: "Make it Fish Eye" },
+];
+
+export const linkNavbar = [
+  { href: "/", label: "About" },
+  { href: "/careers", label: "Careers" },
+  { href: "/events", label: "Events" },
+  { href: "/product", label: "Product" },
+  { href: "/support", label: "Support" },
+];
