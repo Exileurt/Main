85226b27ce992c242c565a8524deaddbecf7a991
Exileurt <favrel.aurelien@gmail.com>
2023-09-18 14:01:40 +0200

    UPDATE GARRY

--- src/Pages/ProjectThree/Footer.jsx
+++ src/Pages/ProjectThree/Footer.jsx
@@ -11,16 +11,16 @@ const linkFooter = [
 
 const Footer = () => {
   return (
-    <div className='bg-twodarkblue'>
-      <img src={logo} alt='logoFooter' className='w-12' />
-      <ul className='flex'>
+    <div className='flex flex-col m-auto bg-twodarkblue/60 items-center'>
+      <img src={logo} alt='logoFooter' className='w-36 m-10' />
+      <ul className='flex font-["Barlow"] space-x-12 font-semibold text-twodarkblue'>
         {linkFooter.map((link, index) => (
-          <li key={index}>
+          <li key={index} className='mb-10'>
             <a href={link.link}>{link.label}</a>
           </li>
         ))}
       </ul>
-      Footer
+      LOGO LOGO LOGO LOGO
     </div>
   );
 };
--- src/Pages/ProjectThree/SectionThree.jsx
+++ src/Pages/ProjectThree/SectionThree.jsx
@@ -5,20 +5,20 @@ const SectionThree = () => {
   return (
     <div className="flex font-['barlow']">
       <div className='relative w-1/2 text-onegreen'>
-        <img src={graphic} alt='graphic' />
+        <img src={graphic} alt='graphic' className='w-full' />
         <span className='absolute bottom-0 py-16 text-center px-44 inset-x-bottom'>
           <h2 className='text-4xl font-bold font-["Fraunces"] pb-10'>
             Graphic Design
           </h2>
           <p className='font-bold'>
             Great design makes you memorable. We deliver artwork that
-            underscores your brand message and captures poetential client s
+            underscores your brand message and captures potential client s
             attention.
           </p>
         </span>
       </div>
       <div className='relative w-1/2 text-onedarkblue'>
-        <img src={photography} alt='photography' />
+        <img src={photography} alt='photography' className='w-full' />
         <span className='absolute bottom-0 py-16 text-center px-44 inset-x-bottom'>
           <h2 className='text-4xl font-bold font-["Fraunces"] pb-10'>
             Photography
